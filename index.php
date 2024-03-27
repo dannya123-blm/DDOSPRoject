ee31270de6c5bf0054be33c17884275745fe68ce
IsraelAdeniran <B00157067@mytudublin.ie>
2024-03-27 12:51:45 +0000

    Changes2

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require 'dbconnections.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style src="css/index.css"></style>
     <title>Whiteboard</title>
 </head>
 <body>
